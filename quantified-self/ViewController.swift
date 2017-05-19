39bfa4c3abe1974010f45ea75b34eea1552e90b8
Austin Wood <austin@austinkwood.com>
2017-05-19 08:21:25 -0700

    Initial Commit

---
+++ quantified-self/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quantified-self
+//
+//  Created by Austin Wood on 5/19/17.
+//  Copyright © 2017 Austin Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
